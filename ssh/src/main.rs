8b4b17b2574dfc9042529214f9f3cc659779ca7e
randymcmillan <randymcmillan@protonmail.com>
2025-05-30 15:30:14 -0400

    ssh/src/lib.rs:initial config

--- ssh/src/main.rs
+++ ssh/src/main.rs
@@ -1,29 +1,6 @@
-use std::sync::Arc;
-
-mod state;
 use env_logger::Env;
-use log::{error, info};
-use state::State;
-use tokio::sync::Mutex;
-
-mod config;
-mod git;
-mod site;
-mod ssh;
-mod utils;
-mod vars;
-
-async fn start() -> anyhow::Result<()> {
-    info!("Loading state...");
-    let state = State::new().await?;
-    let state = Arc::new(Mutex::new(state));
-
-    info!("Starting server...");
-    let _ = sd_notify::notify(true, &[sd_notify::NotifyState::Ready]);
-    ssh::start_server(state).await?;
-    Ok(())
-}
-
+use gnit_ssh::start;
+use log::error;
 #[tokio::main]
 async fn main() {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
